2cfb43f884d9672a5cd065640396b916543f6e31
sodoa <165614968@qq.com>
2016-03-20 22:52:26 +0800

    111111111111111111

--- src/main/java/com/xinfan/wxshop/business/service/OrderNoUtils.java
+++ src/main/java/com/xinfan/wxshop/business/service/OrderNoUtils.java
@@ -1,6 +1,7 @@
 package com.xinfan.wxshop.business.service;
 
 import java.util.Date;
+import java.util.Random;
 
 import org.apache.http.impl.cookie.DateUtils;
 
@@ -8,6 +9,11 @@ public class OrderNoUtils {
 
 	public static String getOrderNo(Integer orderId) {
 		String orderNo = DateUtils.formatDate(new Date(), "yyyyMMddHHmmss") + orderId;
+
+		Random r = new Random();
+		for (int i = 0; i < 3; i++) {
+			orderNo += r.nextInt(9);
+		}
 		return orderNo;
 	}
 
