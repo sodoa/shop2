aaa0cc8abbae8e52172d0fbdafe59b262c2656f6
sodoa <165614968@qq.com>
2016-07-03 13:51:29 +0800

    333333333333333333333333333333

--- WebRoot/json.js
+++ WebRoot/json.js
@@ -1,11 +1,35 @@
 var s = {
-	"jsapi_ticket" : "Cxm_39m0O6z21xOG3NThsJds_QAH1zZXEB0sIbfjkU2JBo5f2VeIEKfpoiREDFucNPhqXvgJOvtyC25CRW2NrfnS2QISZp0CpoZG8BbEqyq5La2Ai1trYEJ9_YIEc_OBCTTdACAQHH",
-	"nonceStr" : "b7cb39a7-ba37-4947-be80-b621f4d75ff1",
-	"signature" : "7ea80ea559e47d3713cf4d416288fe92d0440747",
-	"timestamp" : "1455349813",
-	"url" : "http://www.11grand.cn/rk.html"
-}
-
-ecfe5e7c77bb230615bf21e86855fe41af7fe682
-
-7ea80ea559e47d3713cf4d416288fe92d0440747
+	"data" : {
+		"first" : {
+			"color" : "#173177",
+			"value" : "亲爱的会员，您的会员卡有新积分到账"
+		},
+		"keyword1" : {
+			"color" : "#173177",
+			"value" : "sodo165614968"
+		},
+		"keyword2" : {
+			"color" : "#173177",
+			"value" : "2016-04-10 08:41:09"
+		},
+		"keyword3" : {
+			"color" : "#173177",
+			"value" : "分层积分"
+		},
+		"keyword4" : {
+			"color" : "#173177",
+			"value" : "1.0 元"
+		},
+		"keyword5" : {
+			"color" : "#173177",
+			"value" : "0.0 元"
+		},
+		"remark" : {
+			"color" : "#173177",
+			"value" : "第2级>分销，来至微信sodo165614968"
+		}
+	},
+	"template_id" : "1GQMmWWR0Bh9gzNMdW3nMvubvO-Q9h1UvYno7nF-_0I",
+	"touser" : "oAx3hv0R9bZwwdBYl67CMMbkKrEM",
+	"url" : "http://www.11grand.cn//center/my_center.html"
+};
